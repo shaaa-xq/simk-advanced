da6aa22f70532852fe090cb5942fa184c8d06caf
shaaa-xq <ayeshacha177@gmail.com>
2024-07-14 15:38:26 +0700

    selesaii

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function showPaseenger() {
+const dropdown = document.getElementById("penumpang");
+
+dropdown.addEventListener("click", function () {
   const content = document.querySelector(".dropdown-4");
-}
+  content.classList.toggle("show-dropdown");
+  content.classList.toggle("hide-dropdown");
+});
+
+// function showDropdown() {}
+
+// flatpickr
+// flatpickr("#calendar", {});
